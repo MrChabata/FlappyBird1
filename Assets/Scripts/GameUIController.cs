20c22b0d28a7f0f46f9294831311cc190156c72b
MrChabata <kkostya746@gmail.com>
2023-11-01 19:16:56 +0300

    PauseMenu

--- Assets/Scripts/GameUIController.cs
+++ Assets/Scripts/GameUIController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameUIController : MonoBehaviour
 {
     public GameObject text;
+    public GameObject pausePanel;
     void Start()
     {
     }
@@ -15,5 +17,20 @@ public class GameUIController : MonoBehaviour
         {
             text.SetActive(false);
         }
+
+        if(Input.GetKeyDown(KeyCode.Escape) && pausePanel.activeSelf == false)
+        {
+            pausePanel.SetActive(true);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Continue()
+    {
+        pausePanel.SetActive(false);
     }
 }
